70944868bb678d34a8f72c893253f25f064cabf1
user <user@users-MacBook-Pro.local>
2026-01-17 10:15:22 +0700

    remove dashboard menu

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -12,11 +12,11 @@
   <hr class="sidebar-divider my-0">
 
   <!-- Nav Item - Dashboard -->
-  <li class="nav-item">
+  {{-- <li class="nav-item">
     <a class="nav-link" href="{{ route('dashboard') }}">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Dashboard</span></a>
-  </li>
+  </li> --}}
 
   <li class="nav-item">
     <a class="nav-link" href="{{ route('destination.index') }}">
